5b82aee9e7dcd4306f9afa518dece9e48f49492a
dimadoby4in <dimadoby4in@yandex.ru>
2024-08-12 10:59:06 +0300

    fixed select

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -7,7 +7,7 @@ interface ProductCardProps {
 
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
     return (
-        <div className="flex flex-col gap-2 justify-center items-center p-4 rounded-2xl border-2 border-[#CC7C3220] group hover:border-main">
+        <div className="flex flex-col gap-2 justify-center items-center p-4 rounded-2xl border-2 border-[#CC7C3220] group hover:border-orange">
                 <img className='w-full scale-90  group-hover:scale-100 transition-all duration-300' width={130} loading='lazy' height={130} src={product.image} alt={product.name} />   
             <div className='flex flex-col gap-1 mr-auto'>
                 <h2 className='text-2xl text-main font-bold'>{product.name}</h2>
--- src/components/product/ProductFilter.tsx
+++ src/components/product/ProductFilter.tsx
@@ -18,12 +18,32 @@ const ProductFilter: React.FC<ProductFilterProps> = ({
     onManufacturerChange,
 }) => {
     return (
-        <div className="flex flex-col gap-6 border px-12 py-8 rounded-2xl">
+        <div className="flex flex-col gap-6 border px-12 py-8 rounded-2xl bg-[#FF6A0099]">
+            <div className="relative">
+                <select
+                    value={selectedManufacturer}
+                    onChange={(e) => onManufacturerChange(e.target.value)}
+                    className="appearance-none text-lg font-medium px-4 py-2 pr-8 rounded-lg bg-orange text-white focus:outline-none focus:ring-2 focus:ring-main active:border-main"
+                >
+                    <option value="">Все производители</option>
+                    {manufacturers.map((manufacturer) => (
+                        <option key={manufacturer} value={manufacturer}>
+                            {manufacturer}
+                        </option>
+                    ))}
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+                    <svg className="w-4 h-4 fill-current text-white" viewBox="0 0 20 20">
+                        <path d="M7 10l5 5 5-5H7z" />
+                    </svg>
+                </div>
+            </div>
+
+
             <div className="flex flex-col gap-2">
                 <button
-                    className={`px-4 py-2 rounded ${
-                        selectedCategory === '' ? 'bg-blue-500 text-black' : 'bg-gray-200 text-black'
-                    }`}
+                    className={`px-4 py-2 rounded-lg text-lg font-medium ${selectedCategory === '' ? 'bg-orange text-white' : 'text-black'
+                        }`}
                     onClick={() => onCategoryChange('')}
                 >
                     Все категории
@@ -31,9 +51,8 @@ const ProductFilter: React.FC<ProductFilterProps> = ({
                 {categories.map((category) => (
                     <button
                         key={category}
-                        className={`px-4 py-2 rounded ${
-                            selectedCategory === category ? 'bg-blue-500 text-black' : 'bg-gray-200 text-black'
-                        }`}
+                        className={`px-4 py-2 rounded-lg text-lg font-medium text-black ${selectedCategory === category ? 'bg-orange text-white' : 'text-black'
+                            }`}
                         onClick={() => onCategoryChange(category)}
                     >
                         {category}
@@ -41,18 +60,7 @@ const ProductFilter: React.FC<ProductFilterProps> = ({
                 ))}
             </div>
 
-            <select
-                value={selectedManufacturer}
-                onChange={(e) => onManufacturerChange(e.target.value)}
-                className="px-4 py-2 rounded bg-gray-200"
-            >
-                <option value="">Все производители</option>
-                {manufacturers.map((manufacturer) => (
-                    <option key={manufacturer} value={manufacturer}>
-                        {manufacturer}
-                    </option>
-                ))}
-            </select>
+
         </div>
     );
 };
