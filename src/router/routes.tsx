f22009dbe5271d6b2bc09ecf602b1882594e3978
dimadoby4in <dimadoby4in@yandex.ru>
2024-10-22 14:50:54 +0300

    open routes and edit path

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,11 +1,11 @@
 import Production from "../pages/Production"
-// import Distribution from "../pages/Distribution"
-// import Logistic from "../pages/Logistic"
+import Distribution from "../pages/Distribution"
+import Logistic from "../pages/Logistic"
 
 import React from "react"
 import MainPage from "../pages/MainPage";
-// import Factory from "../pages/Factory";
-// import Policy from "../pages/Policy";
+import Factory from "../pages/Factory";
+import Policy from "../pages/Policy";
 // import Job from "../pages/Job";
 // import Contacts from "../pages/Contacts";
 
@@ -17,12 +17,12 @@ interface RouteConfig {
 
 export const publicRoutes: RouteConfig[] =[ 
     {path: '/', component: <MainPage/> },
-    // {path: '/production', component: <Factory/>},
+    {path: '/production', component: <Factory/>},
     {path: '/production/bread', component: <Production/>},
-    // {path: '/production/confectionery', component: <Production/>},
-    // {path: '/distribution', component: <Distribution/>},
-    // {path: '/logistic', component: <Logistic/>},
-    // {path: '/policy' , component: <Policy/>},
+    {path: '/production/confectionery', component: <Production/>},
+    {path: '/distribution', component: <Distribution/>},
+    {path: '/logistic', component: <Logistic/>},
+    {path: '/policy' , component: <Policy/>},
     // {path: '/job', component: <Job/>},
     // {path: '/contacts', component: <Contacts/>}
 ]
