713983db072c9801097a05809dca1eb23594d89f
dimadoby4in <dimadoby4in@yandex.ru>
2024-10-22 15:42:13 +0300

    fix for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.render( // Use ReactDOM.render for React 17 or below
   <React.StrictMode>
-    <BrowserRouter basename="/DM/">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
